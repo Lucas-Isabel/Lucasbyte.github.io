0d6e35d59fd4427237913a5196349952aca191bc
Lucas Isabel <62727886+Lucasbyte@users.noreply.github.com>
2024-02-12 18:17:52 -0300

    Add

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var produtoCarousel = document.getElementById('produtoCarousel');
-    
-    if (produtoCarousel) {
-        // Adicione um listener para o evento de clique nos botões de navegação
-        produtoCarousel.addEventListener('click', function (event) {
-            if (event.target.classList.contains('button-prev')) {
-                moverCarousel(-1);
-            } else if (event.target.classList.contains('button-next')) {
-                moverCarousel(1);
-            }
-        });
+document.addEventListener("DOMContentLoaded", function () {
+    // Configurações do carrossel
+    const carouselContainer = document.getElementById("produtoCarousel");
+    const produtos = document.querySelectorAll(".produto");
+    const intervaloDeTroca = 2500; // Tempo em milissegundos (5 segundos neste exemplo)
 
-        // Função para mover o carrossel
-        function moverCarousel(direction) {
-            var produtos = produtoCarousel.querySelectorAll('.produto');
-            var tamanhoProduto = produtos[0].offsetWidth + 16; // Considere a largura do produto e a margem
+    let currentIndex = 0;
 
-            // Calcule a nova posição do carrossel
-            var novaPosicao = produtoCarousel.scrollLeft + direction * tamanhoProduto;
-
-            // Anima a rolagem suave
-            produtoCarousel.scrollTo({
-                left: novaPosicao,
-                behavior: 'smooth'
-            });
-
-            // Se atingiu o final do carrossel, volte ao início
-            if (direction === 1 && novaPosicao >= produtoCarousel.scrollWidth - produtoCarousel.clientWidth) {
-                produtoCarousel.scrollTo({
-                    left: 0,
-                    behavior: 'smooth'
-                });
-            } else if (direction === -1 && novaPosicao <= 0) {
-                // Se estiver no início, vá para o final do carrossel
-                produtoCarousel.scrollTo({
-                    left: produtoCarousel.scrollWidth,
-                    behavior: 'smooth'
-                });
-            }
-        }
+    // Função para mostrar o próximo slide
+    function mostrarProximoSlide() {
+        produtos[currentIndex].classList.remove("ativo");
+        currentIndex = (currentIndex + 1) % produtos.length;
+        produtos[currentIndex].classList.add("ativo");
     }
+
+    // Inicia o carrossel automático
+    setInterval(mostrarProximoSlide, intervaloDeTroca);
 });
